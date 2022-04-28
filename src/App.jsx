2e3117a71638fc05cbeb3516c113bc58cb6bd25a
ahlam alshaba <ama1977ln@outlook.com>
2022-04-28 09:49:20 +0200

    everything

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Product from "src/pages/Product";
+import Home from "src/pages/Home";
+import ProductList from "src/pages/ProductList";
+import Register from "src/pages/Register";
+import Login from "src/pages/Login";
+import Cart from "src/pages/Cart";
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
